5046a8dce276f95f3f50803d867df05d47a790a6
kwala16 <rkp600@gmail.com>
2016-05-07 18:50:53 +0530

    version

--- blog/models.py
+++ blog/models.py
@@ -11,4 +11,4 @@ class Post(models.Model):
             self.published_date=timezone.now() 						                           
             self.save()
             def __str__(self):
-            return self.title
+                return self.title
