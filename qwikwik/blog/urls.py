c38a20b264d0bab0ff0feac908bbd78549973da7
rohit <rkp600@gmail.com>
2016-05-16 14:22:33 +0530

    login/logout

--- qwikwik/blog/urls.py
+++ qwikwik/blog/urls.py
@@ -1,6 +1,13 @@
-from django.conf.urls import url
-from . import views
-
-urlpatterns = [
-		url(r'^$', views.post_list, name='post_list'),
-]
+from django.conf.urls import patterns, include, url
+from blog.views import *
+from django.contrib import admin
+ 
+urlpatterns = patterns('',
+    url(r'^admin/',admin.site.urls),
+    url(r'',include('blog.urls')),
+    (r'^logout/$',{'next_page': '/successfully_logged_out/'}),
+    url(r'^accounts/blog/$', 'views.blog'), # If user is not login it will redirect to login page
+    url(r'^register/$','simplesite.views.register'),
+    url(r'^register/success/$','simplesite.views.register.success'),
+    url(r'^home/$', 'simplesite.views.home'),
+)
